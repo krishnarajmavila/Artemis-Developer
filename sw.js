3bfca24e5e25fc5c54dc8938250f5a35142b5125
krishnarajmavila <krishnarajmavila@gmail.com>
2026-04-10 09:48:40 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'artemis2-v2';
+
+// On install: skip waiting so new SW activates immediately
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+// On activate: delete ALL old caches, then claim clients
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
+});
+
+// Network-first strategy: always try network, fall back to cache
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
